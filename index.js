8e6c595714c510f39a7a0caca3c2bee993dc64eb
iamsail <865605793@qq.com>
2017-06-11 16:20:50 +0800

    分文件

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+
+var leftImage = new Vue ({
+    el:"#left",
+    data: {
+        imgClass:'left',
+        categoryClass:'textAll',
+        myTextClass:'leftText',
+        text:'text',
+        message: "前端"
+    }
+});
+
+
+var topImage = new Vue ({
+    el:"#top",
+    data: {
+        imgClass:'top',
+        categoryClass:'textAll',
+        myTextClass:'topText',
+        text:'text',
+        message: "HTML"
+    }
+});
+
+var rightImage = new Vue ({
+    el:"#right",
+    data: {
+        imgClass:'right',
+        categoryClass:'textAll',
+        myTextClass:'rightText',
+        text:'text',
+        message: "CSS"
+    }
+});
+
+var bottomImage = new Vue ({
+    el:"#bottom",
+    data: {
+        imgClass:'bottom',
+        categoryClass:'textAll',
+        myTextClass:'bottomText',
+        text:'text',
+        message: "JavaScript"
+    }
+});
